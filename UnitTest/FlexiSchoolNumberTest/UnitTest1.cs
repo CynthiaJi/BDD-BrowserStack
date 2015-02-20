d327cd5adbdaf95d3f7e8718bdc4117ed1459d21
Cynthiaji <ji.cynthia@gmail.com>
2015-02-19 21:16:46 -0800

    6th commit

---
+++ UnitTest/FlexiSchoolNumberTest/UnitTest1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using flexischool3;
+//https://msdn.microsoft.com/en-us/library/ms182532.aspx#PrepareWalkthrough
+
+
+namespace FlexiSchoolNumberTest
+{
+    [TestClass]
+    public class NumberStringUnitTest
+    {
+        [TestMethod]
+
+        public void NumberStringFizzBuzz()
+        {
+            // arrange
+            int mybeginningnumber = 1;
+            string myprintstr = "what";
+
+            StringAndNumber ba = new StringAndNumber(myprintstr, mybeginningnumber); 
+
+            // act
+            mybeginningnumber = 30;
+            ba.workout(mybeginningnumber);
+             
+            // assert
+           string actual = ba.printStr;
+           Assert.AreEqual("FizzBuzz", actual, "Number can be divided by both 3 and 5");
+        }
+
+        [TestMethod]
+
+        public void NumberStringFizz()
+        {
+            // arrange
+            int mybeginningnumber = 1;
+            string myprintstr = "what";
+
+            StringAndNumber ba = new StringAndNumber(myprintstr, mybeginningnumber);
+
+            // act
+            mybeginningnumber = 3;
+            ba.workout(mybeginningnumber);
+
+            // assert
+            string actual = ba.printStr;
+            Assert.AreEqual("Fizz", actual, "Number can be divided by both 3");
+        }
+
+        [TestMethod]
+
+        public void NumberStringBuzz()
+        {
+            // arrange
+            int mybeginningnumber = 1;
+            string myprintstr = "what";
+
+            StringAndNumber ba = new StringAndNumber(myprintstr, mybeginningnumber);
+
+            // act
+            mybeginningnumber = 10;
+            ba.workout(mybeginningnumber);
+
+            // assert
+            string actual = ba.printStr;
+            Assert.AreEqual("Buzz", actual, "Number can be divided by both  5");
+        }
+        [TestMethod]
+
+        public void NumberStringBeMyself()
+        {
+            // arrange
+            int mybeginningnumber = 1;
+            string myprintstr = "what";
+
+            StringAndNumber ba = new StringAndNumber(myprintstr, mybeginningnumber);
+
+            // act
+            mybeginningnumber = 11;
+            ba.workout(mybeginningnumber);
+
+            // assert
+            string actual = ba.printStr; 
+            Assert.AreEqual(mybeginningnumber.ToString(), actual, "Number can not be divided by 3 and  5");
+        }
+    }
+}
---
+++ UnitTest/flexischool3/stringandnumber.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flexischool3
+{
+ 
+    public class StringAndNumber
+    {
+        private string m_printStr;
+
+        private int m_curnumber;
+
+      
+
+        private StringAndNumber()
+        {
+        }
+
+        public StringAndNumber(string printStr, int curnumber)
+        {
+            m_printStr = printStr;
+            m_curnumber = curnumber;
+        }
+
+        public string printStr
+        {
+            get { return m_printStr; }
+        }
+
+        public int curnumber
+        {
+            get { return m_curnumber; }
+        }
+       
+        public void workout(int amount )
+        {
+            if ((amount % 5 == 0) && (amount % 3 == 0))
+            {
+                m_printStr = "FizzBuzz";
+            }
+            else
+            {
+                if (amount % 5 == 0)
+                {
+                    m_printStr = "Buzz";
+                }
+                else
+                {
+
+                    if (amount % 3 == 0)
+                    {
+                        m_printStr = "Fizz"; ;
+                    }
+                    else
+                    {
+                        m_printStr = amount.ToString();
+                    }
+                }
+            }
+          
+        }
+
+        
+        public static void Main()
+        {
+            StringAndNumber ba = new StringAndNumber("what", 1); 
+
+             for (int j=1; j<=100; j++){
+                 ba.workout(j);
+              
+            Console.WriteLine("Current curnumber is ${0}, print string is ", ba.curnumber.ToString()+ ba.printStr);
+             }
+        }
+
+    }
+}
